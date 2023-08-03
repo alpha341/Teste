18af768a526bcffbf2f02bbf93a19cfded1cba02
unknown <alexlima331@gmail.com>
2023-08-03 10:44:37 +0100

    exemplos aula 1

---
+++ type-number.js
@@ -0,0 +1,34 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 0;
+const alura = "Alura"
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+const total = operacaoMatematica.toFixed(2)
+
+const texto1 = "Olá";
+const texto2 = "mundo!";
+
+const textao = texto1 + " " + texto2;
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(texto1 + " " + texto2)
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade == inputMinusculo);
+
+console.log(cidade.length);
+
